b271d58b1d7a97facd9e8ad9eea513e6f9383fb9
salome sulkhanishvili <55408144+SalomeSulkhanishvili@users.noreply.github.com>
2022-01-08 23:32:46 +0400

    created MealDailyCell general visual

---
+++ WhatToFeed/Tab Bar View Controllers/DailyMealCell/DailyMealCell.swift
@@ -0,0 +1,17 @@
+//
+//  DailyMealCell.swift
+//  WhatToFeed
+//
+//  Created by Salome Sulkhanishvili on 08.01.22.
+//
+
+import UIKit
+
+class DailyMealCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
