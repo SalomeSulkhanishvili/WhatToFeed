fc7ec03754c726b97d95ea2bb665518b848f61d2
salome sulkhanishvili <55408144+SalomeSulkhanishvili@users.noreply.github.com>
2022-01-11 22:35:46 +0400

    added labels for DailyMealView + category collectionView still lott to do :)

---
+++ WhatToFeed/Tab Bar View Controllers/DailyMealViewController/DailyMealCategoryCell/DailyMealCategoryCell.swift
@@ -0,0 +1,19 @@
+//
+//  DailyMealCategoryCell.swift
+//  WhatToFeed
+//
+//  Created by Salome Sulkhanishvili on 11.01.22.
+//
+
+import UIKit
+
+class DailyMealCategoryCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        self.backgroundColor = .lightBlueActionButton
+        self.layer.cornerRadius = 10
+    }
+
+}
--- WhatToFeed/Tab Bar View Controllers/DailyMealViewController/DailyMealMainViewController.swift
+++ WhatToFeed/Tab Bar View Controllers/DailyMealViewController/DailyMealMainViewController.swift
@@ -9,15 +9,19 @@ import UIKit
 
 class DailyMealMainViewController: TabBarMainController {
     
-    private lazy var dailyMealCollectionView: UICollectionView = {
-        loadDailyMealCollectionView()
-    }()
+    private lazy var dailyMealCollectionView: UICollectionView = { loadDailyMealCollectionView() }()
+    private lazy var categoryCollectionView: UICollectionView = { loadCategoryCollectionView() }()
+    private lazy var greetingTitleLabel: UILabel = { loadGreetingTitleLabel() }()
+    private lazy var subTitleLabel: UILabel = { loadSubTitleLabel() }()
+    
+    private let dailyMealCellSize = CGSize(width: 230, height: 400)
+    private let categoryCellSize = CGSize(width: 100, height: 40)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         //self.loadEmptyView(in: self.contentView)
         DispatchQueue.main.async { [weak self] in
-            self?.dailyMealCollectionView.reloadData()
+            self?.loadDailyMealViews()
         }
         // loadController(GeneralMealsAndProductsViewController.load())
     }
@@ -27,47 +31,131 @@ class DailyMealMainViewController: TabBarMainController {
         return viewController
     }
     
-    private func loadDailyMealCollectionView() -> UICollectionView {
-        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .horizontal
-        layout.sectionInset = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 0)
-        layout.itemSize = CGSize(width: 230, height: 400)
-        let collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
+    private func loadDailyMealViews() {
+        contentView.addSubview(dailyMealCollectionView)
+        contentView.addSubview(categoryCollectionView)
+        contentView.addSubview(greetingTitleLabel)
+        contentView.addSubview(subTitleLabel)
+        updateCollectionViewConstraints()
+    }
+    
+    private func loadGreetingTitleLabel() -> UILabel {
+        let title = UILabel()
+        title.numberOfLines = 1
+        title.font = UIFont(name: "Aleo-Bold", size: 13)
+        title.text = "Hello Max charles"
+        title.translatesAutoresizingMaskIntoConstraints = false
+        return title
+    }
+    
+    private func loadSubTitleLabel() -> UILabel {
+        let subTitle = UILabel()
+        subTitle.numberOfLines = 0
+        subTitle.font = UIFont(name: "Aleo-Bold", size: 13)
+        subTitle.text = "are you going to start cooking? you know someone have to cook and I guess you are the one who is willing to sucrifice"
+        subTitle.translatesAutoresizingMaskIntoConstraints = false
+        return subTitle
+    }
+    
+    private func updateCollectionViewConstraints(){
+        NSLayoutConstraint.activate([
+            greetingTitleLabel.topAnchor.constraint(equalTo: contentView.safeTopAnchor,
+                                                    constant: 14),
+            greetingTitleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor,
+                                                        constant: 20),
+            greetingTitleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor,
+                                                         constant: -20)
+        ])
         
+        NSLayoutConstraint.activate([
+            subTitleLabel.topAnchor.constraint(equalTo: greetingTitleLabel.bottomAnchor,
+                                               constant: 7),
+            subTitleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor,
+                                                   constant: 20),
+            subTitleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor,
+                                                    constant: -20)
+        ])
+        
+        NSLayoutConstraint.activate([
+            categoryCollectionView.heightAnchor.constraint(equalToConstant: categoryCellSize.height + 10),
+            categoryCollectionView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            categoryCollectionView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            categoryCollectionView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor,
+                                                           constant: -14)
+        ])
+        
+        NSLayoutConstraint.activate([
+            //TODO: should change top anchor
+            dailyMealCollectionView.topAnchor.constraint(equalTo: subTitleLabel.bottomAnchor,
+                                                         constant: 10),
+            dailyMealCollectionView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            dailyMealCollectionView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            dailyMealCollectionView.bottomAnchor.constraint(equalTo: categoryCollectionView.topAnchor,
+                                                            constant: -22)
+        ])
+        
+        dailyMealCollectionView.reloadData()
+        categoryCollectionView.reloadData()
+    }
+    
+    private func loadCategoryCollectionView() -> UICollectionView {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.itemSize = categoryCellSize
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 0)
+        let collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
+        collectionView.registerCell(withType: DailyMealCategoryCell.self)
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.backgroundColor = .clear
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }
+    
+    private func loadDailyMealCollectionView() -> UICollectionView {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.itemSize = dailyMealCellSize
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 0)
+        let collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
         collectionView.registerCell(withType: DailyMealCell.self)
         
         collectionView.delegate = self
         collectionView.dataSource = self
         collectionView.backgroundColor = .white
         collectionView.showsHorizontalScrollIndicator = false
-        
-        contentView.addSubview(collectionView)
         collectionView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            //TODO: should change top anchor
-            collectionView.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 91),
-            collectionView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor),
-            collectionView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor),
-            collectionView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: -76)
-        ])
         return collectionView
     }
-    
 }
 
 extension DailyMealMainViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5 // just for testing purpose
+        if collectionView == self.dailyMealCollectionView {
+            return 5 // just for testing purpose
+        } else {
+            return 10
+        }
+        
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        if let cell: DailyMealCell = dailyMealCollectionView.dequeueReusableCell(for: indexPath) {
-            cell.load(with: "")
-            return cell
+        if collectionView == self.dailyMealCollectionView {
+            if let cell: DailyMealCell = dailyMealCollectionView.dequeueReusableCell(for: indexPath) {
+                cell.load(with: "")
+                return cell
+            } else {
+                fatalError("incorrect cell for DailyMealCell")
+            }
         } else {
-            fatalError("incorrect cell for DailyMealCell")
+            if let cell: DailyMealCategoryCell = categoryCollectionView.dequeueReusableCell(for: indexPath) {
+                return cell
+            } else {
+                fatalError("incorrect cell for DailyMealCell")
+            }
         }
-        
     }
 }
 
